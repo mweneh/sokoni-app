ac48ca4173dfd792283ef8f24d20eda12ae69b60
David <davidongaroh@gmail.com>
2022-07-26 15:40:26 +0300

    Font-size

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 function Navbar() {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg bg-light">
+      <nav class="navbar navbar-expand-lg bg-light py-3 shadow-sm">
         <div class="container-fluid">
           <a class="navbar-brand fw-bold fs-4" href="#">
             FRESH COLLECTIONS
@@ -20,7 +20,7 @@ function Navbar() {
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#">
                   Home
@@ -28,55 +28,28 @@ function Navbar() {
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">
-                  Link
+                  Products
                 </a>
-              </li>
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu">
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr class="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
               </li>
               <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
+                <a class="nav-link" href="#">
+               About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Contacts
+                </a>
               </li>
             </ul>
-            <form class="d-flex" role="search">
-              <input
-                class="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              ></input>
-              <button class="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
+            <div className="buttons">
+              <a href="" className="btn btn-outline-dark">
+               <i className="fa fa-sign-in me-2"></i> Login</a>
+               <a href="" className="btn btn-outline-dark ms-2">
+               <i className="fa fa-user-plus me-2"></i> Register</a>
+               <a href="" className="btn btn-outline-dark ms-2">
+               <i className="fa fa-cart-plus me-2"></i> Cart(0) </a>
+            </div>
           </div>
         </div>
       </nav>
