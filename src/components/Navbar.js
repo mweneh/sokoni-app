3ff88c28e743bfac93b66f4546b8b4074726c14c
David <davidongaroh@gmail.com>
2022-07-27 23:39:11 +0300

    import useSelector

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Navbar() {
+  const state = useSelector((state) => state.cartHandler)
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-light py-3 shadow-sm">
@@ -49,7 +51,7 @@ function Navbar() {
                <NavLink to="/register" className="btn btn-outline-dark ms-2">
                <i className="fa fa-user-plus me-2"></i> Register</NavLink>
                <NavLink to="/cart" className="btn btn-outline-dark ms-2">
-               <i className="fa fa-cart-plus me-2"></i> Cart(0) </NavLink>
+               <i className="fa fa-cart-plus me-2"></i> Cart({state.length}) </NavLink>
             </div>
           </div>
         </div>
