213ad669add2c8154364717120d8aa9120c1b803
David <davidongaroh@gmail.com>
2022-07-27 14:47:23 +0300

    updating Switch to Routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-
-import './App.css';
-import Home from './components/Home';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Products from "./components/Products";
 
 function App() {
   return (
-    <div className='App'>
-      <Navbar/>
-      <Home />
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route exact path="/products" element={Products}>
+          <Products />
+        </Route>
+        <Route exact path="/" element={Home}>
+          <Home />
+        </Route>
+      </Routes>
     </div>
   );
 }
