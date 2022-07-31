700f50353df2e628b1c828db82a7a9810fbb78e9
David <davidongaroh@gmail.com>
2022-07-31 18:27:33 +0300

    function cleanup

--- src/redux/reducers/carthandler.js
+++ src/redux/reducers/carthandler.js
@@ -1,61 +1,61 @@
 import { Switch } from "react-router-dom"
 
-// const additem=[]
+const additem=[]
 
-// function cartHandler(state=additem,action) {
-//     const product = action.payload
-//     switch (action.type) {
-//         case 'ADDITEM': return [
-//             ...state,
-//             product
-//         ]
-//         break;
-//         case 'DELITEM': 
-//         return state = state.filter(x => {
-//            return x.id !== product.id
-//         })
-//         break;
-//         default: return state
-//     }}
-const cart=[]
-
-function cartHandler(state=cart,action) {
+function cartHandler(state=additem,action) {
     const product = action.payload
     switch (action.type) {
-        case 'ADDITEM':
-            // already in cart?
-            const exists = state.find(i =>i.id === product.id)
-            if(exists){
-                //increment quantity qty
-                return state.map(i =>
-                    i.id === product.id ?{...i, qty: i.qty +1}: i)
-            }
-            else {
-                const product = action.payload
-                return [
-                    ...state,
-                    {
-                        ...product,
-                        qty:1,
-                    }
-                ]
-            }
-            break;
-    case 'DELITEM':
-        const exists2 = state.find(n => n.id === product.id)
-        if(exists2.qty === 1) {
-            return state.filter((n)=>n.id !== exists2.id)
-        }
-        else {
-            return state.map((n)=>
-        n.id === product.id ?{...n, qty: n.qty-1}: n
-        )
-    }
-    break
+        case 'ADDITEM': return [
+            ...state,
+            product
+        ]
+        break;
+        case 'DELITEM': 
+        return state = state.filter(x => {
+           return x.id !== product.id
+        })
+        break;
+        default: return state
+    }}
+// const cart=[]
 
-        default:
-            return state
-            break;
-    }
-}
+// function cartHandler(state=cart,action) {
+//     const product = action.payload
+//     switch (action.type) {
+//         case 'ADDITEM':
+//             // already in cart?
+//             const exists = state.find(i =>i.id === product.id)
+//             if(exists){
+//                 //increment quantity qty
+//                 return state.map(i =>
+//                     i.id === product.id ?{...i, qty: i.qty +1}: i)
+//             }
+//             else {
+//                 const product = action.payload
+//                 return [
+//                     ...state,
+//                     {
+//                         ...product,
+//                         qty:1,
+//                     }
+//                 ]
+//             }
+//             break;
+//     case 'DELITEM':
+//         const exists2 = state.find(n => n.id === product.id)
+//         if(exists2.qty === 1) {
+//             return state.filter((n)=>n.id !== exists2.id)
+//         }
+//         else {
+//             return state.map((n)=>
+//         n.id === product.id ?{...n, qty: n.qty-1}: n
+//         )
+//     }
+//     break
+
+//         default:
+//             return state
+//             break;
+//     }
+// }
 export default cartHandler
